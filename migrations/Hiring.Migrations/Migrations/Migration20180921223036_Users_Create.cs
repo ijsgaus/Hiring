b7f026625ec75e85864436b0197e8fdb3ab3343f
Ivan Simongauz <ijsgaus@gmail.com>
2018-09-21 22:55:59 +0300

    Added migration infrastructure

---
+++ migrations/Hiring.Migrations/Migrations/Migration20180921223036_Users_Create.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Data;
+using FluentMigrator;
+
+namespace Hiring.Migrations.Migrations
+{
+    [Migration(20180921223036)]
+    // ReSharper disable once InconsistentNaming
+    public class Migration20180921223036_Users_Create : Migration
+    {
+        public override void Up()
+        {
+            Create
+                .Table("users")
+                .WithColumn("user_id").AsInt64().PrimaryKey("pk_users")
+                .WithColumn("email").AsString(128).NotNullable()
+                .WithColumn("pwd_hash").AsString(128).NotNullable();
+        }
+
+        public override void Down()
+        {
+            Delete
+                .Table("users");
+        }
+    }
+}
---
+++ migrations/Hiring.Migrations/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using FluentMigrator.Runner;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Npgsql;
+
+namespace Hiring.Migrations
+{
+    class Program
+    {
+        
+            
+        public static void TryCreateDatabase(ILogger logger, string connectionString)
+        {
+            try
+            {
+                logger.LogInformation("TRYING CREATE DATABASE");
+                var bld = new NpgsqlConnectionStringBuilder(connectionString);
+                var database = bld.Database;
+                var userName = bld.Username;
+                bld.Database = "postgres";
+                var serverOnly = bld.ToString();
+                using (var conn = new NpgsqlConnection(serverOnly))
+                {
+                    conn.Open();
+
+                    bool exists;
+                    using (var existsCmd =
+                        new NpgsqlCommand($@"SELECT count(*) from pg_database WHERE datname='{database}'", conn))
+                    {
+                        exists = existsCmd.ExecuteScalar()?.ToString() != "0";
+                    }
+
+                    if (!exists)
+                    {
+                        using (var cmd = new NpgsqlCommand($@"
+                            CREATE DATABASE ""{database}""
+                                WITH OWNER = {userName}
+                                ENCODING = 'UTF8'
+                                TABLESPACE = pg_default
+                                LC_COLLATE = 'ru_RU.utf8'
+                                LC_CTYPE = 'ru_RU.utf8'
+                                CONNECTION LIMIT = -1; ",
+                            conn))
+                        {
+                            cmd.ExecuteNonQuery();
+                        }
+                    }
+                    else
+                    {
+                        logger.LogInformation("Database already exists. Skipping creation.");
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                logger.LogError(e.ToString());
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            var cfg = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .AddEnvironmentVariables()
+                .Build();
+            var postgres = cfg["postgres"];
+
+            var container = new ServiceCollection()
+                .AddLogging(log => log.AddFluentMigratorConsole())
+                .AddFluentMigratorCore()
+                .ConfigureRunner(rb =>
+                {
+                    rb
+                        .AddPostgres()
+                        .WithGlobalConnectionString(postgres)
+                        .ScanIn(typeof(Program).Assembly);
+                })
+                .BuildServiceProvider();
+
+            using (var scope = container.CreateScope())
+            {
+                var provider = scope.ServiceProvider;
+                var log = provider.GetService<ILoggerFactory>().CreateLogger("");
+                TryCreateDatabase(log, postgres);
+                provider.GetRequiredService<IMigrationRunner>().MigrateUp();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
